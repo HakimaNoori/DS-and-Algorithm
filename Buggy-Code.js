a899240f3d0749e8447c2bb287220318262e3165
HakimaNoori <hakima.noori.1379@gmail.com>
2023-10-20 08:56:04 +0430

    Add sulotion for Buggy-Code challenge.

---
+++ Buggy-Code.js
@@ -0,0 +1,10 @@
+function greeting(name) {
+    if (name === "Mobashir") {
+        return "Hello, My Love!";
+    } else {
+        return "Hello, " + name + "!";
+    }
+}
+console.log(greeting("Matt"));
+console.log(greeting("Helen"));
+console.log(greeting("Mobashir"));
