927f791f68b7d5d5bb589c9b4f3390c65ac4d325
HakimaNoori <hakima.noori.1379@gmail.com>
2023-10-22 10:24:57 +0430

    Add isLastCharacterN.js

---
+++ Challenges/isLastCharacterN.js
@@ -0,0 +1,10 @@
+function isLastCharacterN(str){
+    if(str.endsWith("n")){
+        return true;
+    }
+    else return false;
+}
+console.log(isLastCharacterN("Aiden"))
+console.log(isLastCharacterN("Piet"))
+console.log(isLastCharacterN("Bert"))
+console.log(isLastCharacterN("Dean"))
