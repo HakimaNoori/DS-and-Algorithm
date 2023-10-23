cbc74d93f7155db09a37bb93c6becc65d4dce988
HakimaNoori <hakima.noori.1379@gmail.com>
2023-10-23 09:51:23 +0430

    Add the Array-ToString.js

--- Challenges/Array-ToString.js
+++ Challenges/Array-ToString.js
@@ -1,6 +1,6 @@
 function arrayToString(arr){
-    return arr.join("");
-  }
-  console.log(arrayToString([1,2,3,4,5,6]))
-  console.log(arrayToString(["a", "b", "c", "d", "e", "f"]))
-  console.log(arrayToString([1,2,3,"a", "b", "dAAAA"]))
+  return arr.join("");
+}
+console.log(arrayToString([1,2,3,4,5,6]))
+console.log(arrayToString(["a", "b", "c", "d", "e", "f"]))
+console.log(arrayToString([1,2,3,"a", "b", "dAAAA"]))
