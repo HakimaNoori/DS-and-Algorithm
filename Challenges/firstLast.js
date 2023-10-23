9515b8f2ba6148b0251ee2971aac8bbfa2c1921b
HakimaNoori <hakima.noori.1379@gmail.com>
2023-10-23 13:55:16 +0430

    Add the firstLast.js

--- Challenges/firstLast.js
+++ Challenges/firstLast.js
@@ -5,4 +5,8 @@
     return naeStr = first + last
   }
   console.log(firstLast("ganesh"))
+  console.log(firstLast("kali"))
+  console.log(firstLast("shiva"))
+  console.log(firstLast("vishnu"))
+  console.log(firstLast("durga"))
   
