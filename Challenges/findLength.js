cf6f875c077169bc475313dc05f261abab923897
HakimaNoori <hakima.noori.1379@gmail.com>
2023-10-23 14:09:55 +0430

    Add the findLength.js

--- Challenges/findLength.js
+++ Challenges/findLength.js
@@ -1,7 +1,7 @@
 function length(str) {
     return str.length;
 }
-console.log(length("Hakima"))
+console.log(length("Hakima"));
 console.log(length("apple"))
 console.log(length("Make"))
 console.log(length("a"))
