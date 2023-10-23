bc45475fc0c64d5a26c6e7c262f2bfa00b59c23d
HakimaNoori <hakima.noori.1379@gmail.com>
2023-10-23 10:00:59 +0430

    Add the comp.js

--- Challenges/comp.js
+++ Challenges/comp.js
@@ -2,6 +2,6 @@ function comp(str1, str2) {
     if (str1 === str2) return true;
     else return false;
   }
-  console.log(comp("ad", "ad"))
-  console.log(comp("Ali", "ad"))
-  console.log(comp("Fatima", "Fatima"))
+  console.log(comp("ad", "ad"));
+  console.log(comp("Ali", "ad"));
+  console.log(comp("Fatima", "Fatima"));
