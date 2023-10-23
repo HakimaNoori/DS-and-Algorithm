dcce303b82c732fbb7210c6ced2edd01f0dc2d32
HakimaNoori <hakima.noori.1379@gmail.com>
2023-10-23 10:08:31 +0430

    Add the eq.js

--- Challenges/eq.js
+++ Challenges/eq.js
@@ -2,7 +2,7 @@ function eq(str) {
     return eval(str);
   }
   console.log(eq("2 + 4"));
-  console.log(eq("1 + 2"))
+  console.log(eq("1 + 2"));
   console.log(eq("6 / (9 - 7)"))
   console.log(eq("3 + 2 - 4"))
   console.log(eq("2 + 4*12-34+211"))
