3fce56792a7a6763c9abc8b6ac72edf5755a418b
HakimaNoori <hakima.noori.1379@gmail.com>
2023-10-23 12:06:52 +0430

    Add the findIndex.js

--- Challenges/findIndex.js
+++ Challenges/findIndex.js
@@ -5,7 +5,6 @@
   console.log(findIndex(["hi", "edabit", "fgh", "abc"], "fgh"));
   console.log(findIndex(["Red", "blue", "Blue", "Green"], "Blue"));
   console.log(findIndex(["a", "g", "y", "d"], "d"));
-  console.log(findIndex(["12", "1", "34", "5"], "12"));
   console.log(findIndex(["Pineapple", "Orange", "Grape", "Apple"], "Pineapple"));
   
  
