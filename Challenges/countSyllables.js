e6bd3738db8c68c2f1e380f8c6b5a15283b66892
HakimaNoori <hakima.noori.1379@gmail.com>
2023-10-24 15:32:26 +0430

    Add the stringInt.js

--- Challenges/countSyllables.js
+++ Challenges/countSyllables.js
@@ -1,4 +1,6 @@
 function countSyllables(word) {
     return word.length / 2;
   }
- 
+  console.log(countSyllables("Lalalalalalala"))
+  console.log(countSyllables("Hehehehehehehehehehe"))
+  console.log(countSyllables("NANANANA"))
