de9cd37091d1d032d9612edb4c3c71836f7c09ec
HakimaNoori <hakima.noori.1379@gmail.com>
2023-10-24 17:24:57 +0430

    Add the Char-to-ASCLL.js

--- Challenges/Char-to-ASCLL.js
+++ Challenges/Char-to-ASCLL.js
@@ -2,6 +2,6 @@ function ctoa(str){
     return str.charCodeAt();
   }
   console.log(ctoa("A"));
-  console.log(ctoa("M"))
+  console.log(ctoa("M"));
   console.log(ctoa("/"))
   console.log(ctoa("]"))
