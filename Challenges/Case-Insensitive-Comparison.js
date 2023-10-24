e37cc0f34f0de9068261313e0e2a78c921e0a0b4
HakimaNoori <hakima.noori.1379@gmail.com>
2023-10-24 17:47:00 +0430

    Add the Case-Insensitive-Comparison.js

--- Challenges/Case-Insensitive-Comparison.js
+++ Challenges/Case-Insensitive-Comparison.js
@@ -6,5 +6,5 @@ function match(str1, str2){
   }
   console.log(match("hello", "HELLO"));
   console.log(match("motive", "emotive"))
-  console.log(match("venom", "VENOM"))
+  console.log(match("venom", "VENOM"));
   console.log(match("mask", "mAskinG"));
