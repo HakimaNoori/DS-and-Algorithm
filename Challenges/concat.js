75efac8357527096b8378f21e166989bcb127e83
HakimaNoori <hakima.noori.1379@gmail.com>
2023-10-26 22:46:55 +0430

    Add the concat.js

--- Challenges/concat.js
+++ Challenges/concat.js
@@ -1,3 +1,7 @@
 function concat(arr1, arr2){
     return arr1.concat(arr2);
 }
+console.log(concat([1, 2, 3, 4], [7, 8 ,9]));
+console.log(concat([1, 13, 23, 4], [17, 68 ,9]));
+console.log(concat(["Ali", "Amir"], [7, 8 ,9]));
+console.log(concat(["Sara" ,"Ali", "samir"], ["Zahra", "Hakima", "Sana"]));
