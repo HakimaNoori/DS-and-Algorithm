7c78f169eda1a7a31dbbaa4a22ef87c6566ec48a
HakimaNoori <hakima.noori.1379@gmail.com>
2023-10-26 23:01:49 +0430

    Add the arrayToString.js

--- Challenges/arrayToString.js
+++ Challenges/arrayToString.js
@@ -2,6 +2,3 @@ function arrayToString(arr){
     return arr.join("");
 }
 console.log(arrayToString([1,2,3,4,5,6,7]));
-console.log(arrayToString(["Ali", "Smir", "Sara"]));
-console.log(arrayToString(["a", "b", "q", 4,5,6,7]));
-console.log(arrayToString(["ASDF", "aswqe", "1233"]));
